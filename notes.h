36639e0476c9df4badd38aaee9a5026757c4b8f7
Mihir Srivastava <srimihirprakhar@gmail.com>
2022-08-05 18:48:36 +0530

    Piano CPP Library

---
+++ notes.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include<windows.h>
+
+//0 
+void A0(int time, int sleep){      Beep(27.5, time); Sleep(sleep);}
+void A0S(int time, int sleep){     Beep(29.135, time); Sleep(sleep);}
+void B0(int time, int sleep){      Beep(30.868, time); Sleep(sleep);}
+
+//1
+void C1(int time, int sleep){     Beep(32.703, time); Sleep(sleep);}
+void C1S(int time, int sleep){    Beep(34.648, time); Sleep(sleep);}
+void D1(int time, int sleep){    Beep(36.708, time); Sleep(sleep);}
+void D1S(int time, int sleep){    Beep(38.891, time); Sleep(sleep);}
+void E1(int time, int sleep){    Beep(41.203, time); Sleep(sleep);}
+void F1(int time, int sleep){    Beep(43.654, time); Sleep(sleep);}
+void F1S(int time, int sleep){    Beep(46.249, time); Sleep(sleep);}
+void G1(int time, int sleep){    Beep(48.999, time); Sleep(sleep);}
+void G1S(int time, int sleep){    Beep(51.913, time); Sleep(sleep);}
+void A1(int time, int sleep){    Beep(55.000, time); Sleep(sleep);}
+void A1S(int time, int sleep){    Beep(58.270, time); Sleep(sleep);}
+void B1(int time, int sleep){    Beep(61.735, time); Sleep(sleep);}
+
+//2
+void C2(int time, int sleep){     Beep(65.406, time); Sleep(sleep);}
+void C2S(int time, int sleep){    Beep(69.296, time); Sleep(sleep);}
+void D2(int time, int sleep){    Beep(73.416, time); Sleep(sleep);}
+void D2S(int time, int sleep){    Beep(77.782, time); Sleep(sleep);}
+void E2(int time, int sleep){    Beep(82.407, time); Sleep(sleep);}
+void F2(int time, int sleep){    Beep(87.307, time); Sleep(sleep);}
+void F2S(int time, int sleep){    Beep(92.499, time); Sleep(sleep);}
+void G2(int time, int sleep){    Beep(97.999, time); Sleep(sleep);}
+void G2S(int time, int sleep){    Beep(103.83, time); Sleep(sleep);}
+void A2(int time, int sleep){    Beep( 110.00, time); Sleep(sleep);}
+void A2S(int time, int sleep){    Beep(116.54, time); Sleep(sleep);}
+void B2(int time, int sleep){    Beep(123.47, time); Sleep(sleep);}
+
+//3
+void C3(int time, int sleep){     Beep(130.81, time); Sleep(sleep);}
+void C3S(int time, int sleep){    Beep(138.59, time); Sleep(sleep);}
+void D3(int time, int sleep){    Beep(146.83, time); Sleep(sleep);}
+void D3S(int time, int sleep){    Beep(155.56, time); Sleep(sleep);}
+void E3(int time, int sleep){    Beep(164.81, time); Sleep(sleep);}
+void F3(int time, int sleep){    Beep(174.61, time); Sleep(sleep);}
+void F3S(int time, int sleep){    Beep(185.00, time); Sleep(sleep);}
+void G3(int time, int sleep){    Beep(196.00, time); Sleep(sleep);}
+void G3S(int time, int sleep){    Beep(207.65, time); Sleep(sleep);}
+void A3(int time, int sleep){    Beep(220.00, time); Sleep(sleep);}
+void A3S(int time, int sleep){    Beep(233.08, time); Sleep(sleep);}
+void B3(int time, int sleep){    Beep(246.94, time); Sleep(sleep);}
+
+//4
+void C4(int time, int sleep){     Beep(261.63, time); Sleep(sleep);}
+void C4S(int time, int sleep){    Beep(277.18, time); Sleep(sleep);}
+void D4(int time, int sleep){    Beep(293.66, time); Sleep(sleep);}
+void D4S(int time, int sleep){    Beep(311.13, time); Sleep(sleep);}
+void E4(int time, int sleep){    Beep(329.63, time); Sleep(sleep);}
+void F4(int time, int sleep){    Beep(349.23, time); Sleep(sleep);}
+void F4S(int time, int sleep){    Beep(369.99, time); Sleep(sleep);}
+void G4(int time, int sleep){    Beep(392.00, time); Sleep(sleep);}
+void G4S(int time, int sleep){    Beep(415.30, time); Sleep(sleep);}
+void A4(int time, int sleep){    Beep(440.00, time); Sleep(sleep);}
+void A4S(int time, int sleep){    Beep(466.16, time); Sleep(sleep);}
+void B4(int time, int sleep){    Beep(493.88, time); Sleep(sleep);}
+
+//5
+void C5(int time, int sleep){     Beep(523.25, time); Sleep(sleep);}
+void C5S(int time, int sleep){    Beep(554.37, time); Sleep(sleep);}
+void D5(int time, int sleep){    Beep(587.33, time); Sleep(sleep);}
+void D5S(int time, int sleep){    Beep(622.25, time); Sleep(sleep);}
+void E5(int time, int sleep){    Beep(659.25, time); Sleep(sleep);}
+void F5(int time, int sleep){    Beep(698.46, time); Sleep(sleep);}
+void F5S(int time, int sleep){    Beep(739.99, time); Sleep(sleep);}
+void G5(int time, int sleep){    Beep(783.99, time); Sleep(sleep);}
+void G5S(int time, int sleep){    Beep(830.61, time); Sleep(sleep);}
+void A5(int time, int sleep){    Beep(880.00, time); Sleep(sleep);}
+void A5S(int time, int sleep){    Beep(932.33, time); Sleep(sleep);}
+void B5(int time, int sleep){    Beep(987.77, time); Sleep(sleep);}
+
+//6
+void C6(int time, int sleep){     Beep(1046.5, time); Sleep(sleep);}
+void C6S(int time, int sleep){    Beep(1108.7, time); Sleep(sleep);}
+void D6(int time, int sleep){    Beep(1174.7, time); Sleep(sleep);}
+void D6S(int time, int sleep){    Beep(1244.5, time); Sleep(sleep);}
+void E6(int time, int sleep){    Beep(1318.5, time); Sleep(sleep);}
+void F6(int time, int sleep){    Beep(1396.9, time); Sleep(sleep);}
+void F6S(int time, int sleep){    Beep(1480.0, time); Sleep(sleep);}
+void G6(int time, int sleep){    Beep(1568.0, time); Sleep(sleep);}
+void G6S(int time, int sleep){    Beep(1661.2, time); Sleep(sleep);}
+void A6(int time, int sleep){    Beep(1760.0, time); Sleep(sleep);}
+void A6S(int time, int sleep){    Beep(1864.7, time); Sleep(sleep);}
+void B6(int time, int sleep){    Beep(1979.5, time); Sleep(sleep);}
+
+//7
+void C7(int time, int sleep){     Beep(2093.0, time); Sleep(sleep);}
+void C7S(int time, int sleep){    Beep(2217.5, time); Sleep(sleep);}
+void D7(int time, int sleep){    Beep(2349.3, time); Sleep(sleep);}
+void D7S(int time, int sleep){    Beep(2489.0, time); Sleep(sleep);}
+void E7(int time, int sleep){    Beep(2637.0, time); Sleep(sleep);}
+void F7(int time, int sleep){    Beep(2793.8, time); Sleep(sleep);}
+void F7S(int time, int sleep){    Beep(2960.0, time); Sleep(sleep);}
+void G7(int time, int sleep){    Beep(3136.0, time); Sleep(sleep);}
+void G7S(int time, int sleep){    Beep(3322.4, time); Sleep(sleep);}
+void A7(int time, int sleep){    Beep(3520.0, time); Sleep(sleep);}
+void A7S(int time, int sleep){    Beep(3729.3, time); Sleep(sleep);}
+void B7(int time, int sleep){    Beep(3951.1, time); Sleep(sleep);}
+
+//8
+void C8(int time, int sleep){    Beep(4186.0, time);  Sleep(sleep);}
